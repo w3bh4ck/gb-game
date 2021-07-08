db4c2764f4f4a41da5808355f1c97bc9b4c7753a
w3bh4ck <lucky@softcom.ng>
2021-07-08 12:14:51 +0100

    Core(_): implemented functions for all wining conditions

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,12 @@ import { CARD_DATA } from "../src/_data/card.data";
 import GridBox from "../src/components/grid-box"
 
 export default function Home() {
+	// const [suffledTiles, setShuffledTiles]
+	const [winingStreak, setWinningStreak] = useState(0);
 	const [boardTrackingArray, setBoardTrackingArray] = useState([
 		[0, 0, 0, 0, 0],
 		[0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0],
+		[0, 0, 1, 0, 0],
 		[0, 0, 0, 0, 0],
 		[0, 0, 0, 0, 0]]);
 
@@ -18,8 +20,74 @@ export default function Home() {
 
 		//Update the board tracking array
 		let copyBoardArray = [...boardTrackingArray];
-		copyBoardArray[row - 1][col - 1] = 1;
-		setBoardTrackingArray(copyBoardArray)
+		if (copyBoardArray[row - 1][col - 1] === 1) {
+			copyBoardArray[row - 1][col - 1] = 0;
+		} else {
+			copyBoardArray[row - 1][col - 1] = 1;
+		}
+		setBoardTrackingArray(copyBoardArray);
+
+		//Check winning criteria
+		checkWinningCriteria();
+	}
+
+	/**
+		* @description: Check if winning condition has been met on every selection
+		*/
+	const checkWinningCriteria = () => {
+		let copyBoardArray = [...boardTrackingArray];
+		checkRowWins(copyBoardArray);
+		checkColumnWins(copyBoardArray);
+		checkDiagonalWin(copyBoardArray)
+	}
+
+
+	/**
+		* @description| check if the row winning condition has been met
+		* @param array 
+		*/
+	const checkRowWins = (array) => {
+		let copyBoardArray = [...array];
+		let rowReducer = (accumulator, currentValue) => accumulator + currentValue;
+		copyBoardArray.forEach(row => {
+			if (row.reduce(rowReducer) === 5) {
+				setWinningStreak(winingStreak + 1);
+			}
+		})
+	}
+
+	/**
+		*  @description| check if the row winning condition has been met
+		* @param array 
+		*/
+	const checkColumnWins = (array) => {
+		let copyBoardArray = [...array]
+		let transposedArray = copyBoardArray[0].map((x, i) => copyBoardArray.map(x => x[i]))
+		checkRowWins(transposedArray);
+	}
+
+	/**
+		* @description| check if the diagonal winning condition has been met
+		* @param array 
+		*/
+	const checkDiagonalWin = (array) => {
+		//check diagonal right to left
+		let leftBaseNumber = 0;
+		let rightBaseNumber = 4;
+		let rightDiagonalTotal = 0;
+		let leftDiagonalTotal = 0;
+		for (let x = 0; x < array.length; x++) {
+			leftDiagonalTotal = leftDiagonalTotal + array[x][rightBaseNumber];
+			leftBaseNumber++
+		}
+		for (let y = 0; y < array.length; y++) {
+			console.log("diagonal", rightDiagonalTotal + array[y][rightBaseNumber]);
+			rightDiagonalTotal = rightDiagonalTotal + array[y][rightBaseNumber];
+			rightBaseNumber--
+		}
+		if (leftDiagonalTotal === 5 || rightDiagonalTotal === 5) {
+			setWinningStreak(winingStreak + 1);
+		}
 	}
 
 	return (
@@ -32,6 +100,7 @@ export default function Home() {
 		</StyleMainWrapper>
 	);
 }
+
 
 const StyleGridLayout = styled.div`
 	display: grid;
