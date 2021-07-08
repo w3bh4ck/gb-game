9a3cb1106551a6468028923ff3a738b4304f6b82
w3bh4ck <lucky@softcom.ng>
2021-07-08 13:41:59 +0100

    Core(_): Add styling for the selected cards

--- src/components/grid-box/index.tsx
+++ src/components/grid-box/index.tsx
@@ -2,30 +2,31 @@ import styled from 'styled-components';
 import React, { FC } from 'react';
 
 interface Iprops {
- label: string;
- index: number;
- onClickCard: (index: number) => void
+  label: string;
+  index: number;
+  onClickCard: (index: number, label: string) => void,
+  selectedTiles: Array<string>
 }
 
 const formatId = (index: number): string => {
- let row = Math.ceil((index + 1) / 5);
- if (index + 1 > 0)
-  return `R${row}-T${index + 1}`
+  let row = Math.ceil((index + 1) / 5);
+  if (index + 1 > 0)
+    return `R${row}-T${index + 1}`
 }
 
-const GridBox: FC<Iprops> = ({ label, index, onClickCard }) => {
- return (
-  <StyleBox onClick={() => onClickCard(index)}>
-   <NumberWrapper>
-    <span className='number'>{formatId(index)}</span>
-   </NumberWrapper>
-   <LabelWrapper>
-    <span className='label'>
-     {label}
-    </span>
-   </LabelWrapper>
-  </StyleBox>
- );
+const GridBox: FC<Iprops> = ({ label, index, onClickCard, selectedTiles }) => {
+  return (
+    <StyleBox className={selectedTiles.includes(label) ? "selected" : ""} onClick={() => onClickCard(index, label)}>
+      <NumberWrapper>
+        <span className='number'>{formatId(index)}</span>
+      </NumberWrapper>
+      <LabelWrapper>
+        <span className={selectedTiles.includes(label) ? 'label selected' : "label"}>
+          {label}
+        </span>
+      </LabelWrapper>
+    </StyleBox>
+  );
 };
 
 export default GridBox;
@@ -35,6 +36,13 @@ const StyleBox = styled.div`
   height: 6rem;
   border-radius: 0.2rem;
   cursor: pointer;
+  .selected{
+    border: 2px solid #006600;
+    border-radius: 50%;
+    color: white;
+    opacity: 0.5;
+  background-image: linear-gradient(to right, #006600,  #003300);
+  }
   @media (max-width: 768px) {
     font-size: 11px;
     height: 3rem;
@@ -47,12 +55,12 @@ const LabelWrapper = styled.div`
   display: flex;
   justify-content: center;
   .label{
-   color: #ccc;
    padding: 1rem ;
   }
    @media (max-width: 768px) {
   .label{
    padding: 0.1rem ;
+   font-size: 9px;
   }
   }
 `;
@@ -60,7 +68,9 @@ const NumberWrapper = styled.div`
   display: flex;
   justify-content: end;
   .number{
-   color: #ccc;
    padding: 0.2rem;
+  }
+  @media (max-width: 768px) {
+    font-size: 7px;
   }
 `;
